4ed969e668dca984ddcc9c62f82db602ce0c8312
Pepe Miyashiro <jose.miyashiro@gmail.com>
2015-07-18 17:07:02 -0500

    Bootstrap Removed

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,12 +1,2 @@
 global.jQuery = require('./vendor/jquery-1.11.3');
 var $ = jQuery;
-var bootstrap = require('./vendor/bootstrap');
-var sliders = require('./inc/sliders');
-
-
-/**
- * Initicia el módulo sliders
- * @type {Slider}
- */
-
-sliders.init();
