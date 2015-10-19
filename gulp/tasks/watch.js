086c1bc333deb6b6ee95918e7443a6587e2dffbc
Pepe Miyashiro <jose.miyashiro@gmail.com>
2015-10-19 13:05:20 -0500

    se hizo update de los task para el watch y los mixins de sprites

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,10 +1,10 @@
 var config 		= require('../config.js'),
 		gulp 			= require('gulp');
 
-gulp.task('watch', ['browserSync'], function() {
+gulp.task('watch', function() {
     
     gulp.watch( config.jade.source + '/**/*.jade', ['jade'] );
-    gulp.watch( config.stylus.source + '/**/*.{styl,css}', ['stylus'] );
+    gulp.watch( config.stylus.source + '/**/*.styl', ['stylus'] );
     gulp.watch( config.javascript.source + '/**/*.js', ['javascript'] );
 
 });
